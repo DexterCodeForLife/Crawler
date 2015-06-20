735110685cd07fbce3a0b1b47abd1a6cb8aa6303
Justus Flerlage <justusflerlage@taista.net>
2015-06-20 04:53:32 -0700

    change example template

--- Source/Example/Main.cpp
+++ Source/Example/Main.cpp
@@ -1,7 +1,73 @@
 #include <Crawler.hpp>
 #include <iostream>
 
-int main ( int argc , char ** argv )
+class Application : public Crawler::Application
 {
-	return 0 ;
-}
+	public :
+		bool onStartup ( )
+		{
+			return Crawler::Application::onStartup ( ) ;
+		}
+		
+		void onMain ( )
+		{
+			Crawler::Application::onMain ( ) ;
+		}
+		
+		void onShutdown ( )
+		{
+			Crawler::Application::onShutdown ( ) ;
+		}
+		
+		void onSpawnWorker ( Crawler::Worker & worker )
+		{
+			Crawler::Application::onSpawnWorker ( worker ) ;
+		}
+		
+		void onDespawnWorker ( Crawler::Worker & worker )
+		{
+			Crawler::Application::onDespawnWorker ( worker ) ;
+		}
+		
+		void onUnsupportedProtocol ( Crawler::Worker & worker , Crawler::Website & website )
+		{
+			Crawler::Application::onUnsupportedProtocol ( worker , website ) ;
+		}
+		
+		bool onRequest ( Crawler::Worker & worker , Crawler::Website & website , Crawler::Link & link , sf::Http & client , sf::Http::Request & request )
+		{
+			return Crawler::Application::onRequest ( worker , website , link , client , request ) ;
+		}
+
+		bool onResponse ( Crawler::Worker & worker , Crawler::Website & website , Crawler::Link & link , sf::Http & client , sf::Http::Response & response )
+		{
+			return Crawler::Application::onResponse ( worker , website , link , client , response ) ;
+		}
+			
+		bool onParsing ( Crawler::Worker & worker , Crawler::Website & website , Crawler::Link & link , pugi::xml_document & document )
+		{
+			return Crawler::Application::onParsing ( worker , website , link , document ) ;
+		}
+		
+		bool onReportLink ( const std::string & string )
+		{
+			return Crawler::Application::onReportLink ( string ) ;
+		}
+		
+		bool onAddWebsite ( Crawler::Website & website )
+		{
+			return Crawler::Application::onAddLink ( website ) ;
+		}
+		
+		bool onAddLink ( Crawler::Website & website , Crawler::Link & link )
+		{
+			return Crawler::Application::onAddLink ( website , link ) ;
+		}
+		
+		void onVisitedWebsites ( )
+		{
+			Crawler::Application::onVisitedWebsites ( ) ;
+		}
+} ;
+
+CRAWLER_APPLICATION ( Application )
