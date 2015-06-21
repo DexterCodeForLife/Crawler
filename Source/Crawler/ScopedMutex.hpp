00625cbddc2ad669907bc636d3aef0ff23206077
Justus Flerlage <justusflerlage@taista.net>
2015-06-21 01:38:03 -0700

    prohibited copying and moving scoped mutex

--- Source/Crawler/ScopedMutex.hpp
+++ Source/Crawler/ScopedMutex.hpp
@@ -12,8 +12,30 @@ namespace Crawler
 	class CRAWLER_API ScopedMutex
 	{
 		public :
+			/// Default constructor
+			/// \param mutex Reference to mutex which is used
 			ScopedMutex ( sf::Mutex & mutex ) ;
+			
+			/// Default destructor
 			~ScopedMutex ( ) ;
+			
+			/// \brief Copy constructor
+			/// \param mutex ScopedMutex which is copied
+			ScopedMutex ( const Worker & mutex ) = delete ;
+			
+			/// \brief Move constructor
+			/// \param mutex ScopedMutex which is moved
+			ScopedMutex ( ScopedMutex && mutex ) = delete ;
+			
+			/// \brief Copy assignment operator
+			/// \param mutex ScopedMutex which is copied
+			/// \return Reference to this
+			ScopedMutex & operator = ( const ScopedMutex & mutex ) = delete ;
+			
+			/// \brief Move assignment operator
+			/// \param mutex ScopedMutex which is moved
+			/// \return Reference to this
+			ScopedMutex & operator = ( ScopedMutex && mutex ) = delete ;
 			
 		private :
 			sf::Mutex & mutex ;
